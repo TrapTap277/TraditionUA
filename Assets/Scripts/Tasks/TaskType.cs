cd9029d8a2aeb6849d08790f3ca8b93fc62e5cba
Ihor Sichkar <47111877+BelladonDeveloper@users.noreply.github.com>
2024-01-11 19:50:19 +0200

    Create ITaskManager interface

---
+++ Assets/Scripts/Tasks/TaskType.cs
@@ -0,0 +1,23 @@
+namespace Tasks
+{
+    /// <summary>
+    /// Each level will have own group of task`s type
+    /// I think 10 tasks for a level is enough
+    /// </summary>
+    public enum TaskType
+    {
+        None = 0,
+
+        // Temporary names - please, rename it before use
+        FirstTask = 1,
+        SecondTask,
+
+        TaskInNextLevel = 10,
+
+
+
+
+        // Should be removed later
+        TemporaryType = 999
+    }
+}
