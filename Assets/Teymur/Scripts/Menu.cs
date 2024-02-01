d61c6b8ac4d2987d97c42157b932645d5d721d0f
Aaxtroence <aaxtroencexd@gmail.com>
2024-02-01 20:43:45 +0200

    Menu script
    немного подправил скрипт "Menu"

--- Assets/Teymur/Scripts/Menu.cs
+++ Assets/Teymur/Scripts/Menu.cs
@@ -3,89 +3,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Menu : MonoBehaviour, IManager
+public class Menu : MonoBehaviour
 {
-    public Button settingsButton;
-    public Button newGameButton;
-    public Button continueButton;
-    public Button levelChanger;
-    public Button exitButton;
-
-    private string currentSceneName;
-
-    public void Init()
-    {
-        Debug.Log("I am initialized");
-    }
+    public Button SettingsButton;
+    public Button PlayButton;
+    public Button ExitButton;
 
     private void Start()
     {
-        settingsButton.onClick.AddListener(OpenSettings);
-        levelChanger.onClick.AddListener(ShowLevelChanger);
-        exitButton.onClick.AddListener(ExitGame);
-        
-        currentSceneName = SceneManager.GetActiveScene().name;
-
-        UpdateMenus();
-    }
-
-    private void ShowLevelChanger()
-    {
-        Register.Get<UIManager>().Show(UIPopupType.LevelChanger);
+        SettingsButton.onClick.AddListener(OpenSettings);
+        ExitButton.onClick.AddListener(ExitGame);
     }
 
     private void OpenSettings()
     {
-        Register.Get<UIManager>().Show(UIPopupType.POPUP1);
+        Debug.Log("OpenSettings");
     }
-
-    private void UpdateMenus()
-    {
-        settingsButton.gameObject.SetActive(false);
-        newGameButton.gameObject.SetActive(false);
-        continueButton.gameObject.SetActive(false);
-        exitButton.gameObject.SetActive(false);
-
-        if (currentSceneName == "MainMenu")
-        {
-            newGameButton.gameObject.SetActive(true);
-            continueButton.gameObject.SetActive(true);
-            exitButton.gameObject.SetActive(true);
-
-            if (PlayerPrefs.HasKey("LastSaved"))
-            {
-                settingsButton.gameObject.SetActive(true);
-            }
-        }
-        else if (currentSceneName == "SettingsMenu")
-        {
-            exitButton.gameObject.SetActive(true);
-        }
-        else if (currentSceneName == "LevelsMenu")
-        {
-            exitButton.gameObject.SetActive(true);
-        }
-        else if (currentSceneName == "CreditsMenu")
-        {
-            exitButton.gameObject.SetActive(true);
-        }
-        else if (currentSceneName == "TutorialMenu")
-        {
-            exitButton.gameObject.SetActive(true);
-        }
-    }
-
     private void ExitGame()
     {
+        Debug.Log("Exit");
         Application.Quit();
     }
-
-    public void Dispose()
-    {
-        Debug.Log("I am Disposed");
-        settingsButton.onClick.RemoveAllListeners();
-        levelChanger.onClick.RemoveAllListeners();
-        exitButton.onClick.RemoveAllListeners();
-    }
+    
 }
 
