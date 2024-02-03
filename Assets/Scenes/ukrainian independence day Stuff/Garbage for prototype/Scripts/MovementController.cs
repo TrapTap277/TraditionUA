663146aecfc03b679cbe1d7fe24ef59f37288c6b
Aaxtroence <aaxtroencexd@gmail.com>
2024-02-03 21:50:31 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/BelladonDeveloper/TraditionUA""
    This reverts commit 21351e53332304f607d7b105ffae36a2471b989a.

--- Assets/Scenes/ukrainian independence day Stuff/Garbage for prototype/Scripts/MovementController.cs
+++ Assets/Scenes/ukrainian independence day Stuff/Garbage for prototype/Scripts/MovementController.cs
@@ -7,7 +7,7 @@ public class MovementController : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private Rigidbody _rb;
-    [SerializeField] private bool _canMove;
+    private bool _canMove;
     private Vector3 moveDirectionZ;
 
     private void Start()
